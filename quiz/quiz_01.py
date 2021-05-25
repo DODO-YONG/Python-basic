c0ab87fcc86b0cfe5dc83b7af9fc2398926c61b9
JSY <puppy3987@gmail.com>
2021-05-25 10:48:59 +0900

    open with as

--- quiz/quiz_01.py
+++ quiz/quiz_01.py
@@ -43,6 +43,54 @@ def quiz_6():
     sort_lst = sorted(lst)
     print(sort_lst)
     print(len(sort_lst))
+# 리스트와 사전을 다루는 퀴즈입니다.
+# 파일명은 quiz01_3.py로 저장합니다
+# 다음과 같이 사전을 포함한 리스트가 선언되어 있습니다.
+#
+# 두 학생의 kor, eng, math 합계 점수와 평균을 사전 데이터에 "total", "average" 키값으로 넣어 봅시다.
+
+students = [
+    {
+        "name": "Kim",
+        "kor": 80,
+        "eng": 90,
+        "math": 80
+    },
+    {
+        "name": "Lee",
+        "kor": 90,
+        "eng": 85,
+        "math": 85
+    }
+]
+
+def quiz_1_3():
+    # print(students.values())
+    print(len(students))
+
+    for i in range(len(students)):
+        dict_total = 0
+        count = 0
+        for n,s in students[i].items():
+            if isinstance(s, int):
+                count += 1
+                dict_total += s
+
+        students[i].update({"total": dict_total})
+        students[i].update({"average": round(dict_total/count,2)})
+    print(students)
+
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def quiz_1_2():
+    slice = lst[3:7]
+    slice.reverse()
+    lst[3:7] = slice
+
+    print(lst)
+
 
 if __name__ == "__main__":
     quiz_1()
@@ -51,3 +99,5 @@ if __name__ == "__main__":
     quiz_4()
     quiz_5()
     quiz_6()
+    quiz_1_2()
+    quiz_1_3()
--- quiz/quiz_02.py
+++ quiz/quiz_02.py
@@ -10,14 +10,7 @@
 # 리스트로부터 [4, 5, 6, 7]을 추출하여 slice 에 담아 봅시다.
 # slice의 순서를 뒤집어 봅시다.
 # lst의 적절한 부분에 slice를 끼워 넣어 봅시다.
-lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-def quiz_1():
-    slice = lst[3:7]
-    slice.reverse()
-    lst[3:7] = slice
 
-    print(lst)
-    
 
 if __name__ == "__main__":
     quiz_1()
